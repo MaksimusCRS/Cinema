6c486471ff4b4d555450596e1e69636700875b6b
Maks <92159085+MaksimusCRS@users.noreply.github.com>
2021-10-11 13:40:54 +0300

    Тело
    Механизм цены

---
+++ Cinema/JS/script.js
@@ -0,0 +1,14 @@
+const schemeSvg = document.querySelector('.scheme svg');
+const totalPriceTag = document.querySelector(".price-total");
+let cost = 500;
+let totalPrice = 0;
+schemeSvg.addEventListener("click", (event) => {
+    if (!event.target.classList.contains('booked')) {
+        event.target.classList.toggle('active');
+        let totalSeats = schemeSvg.querySelectorAll('.active').length
+        totalPrice = totalSeats * cost;
+        totalPriceTag.textContent = totalPrice;
+        console.log();
+    }
+
+});
